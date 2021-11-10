ffa8759d0ac0c771918d0bd39e8265d971554ea3
Luis Molina <luismateo2416@gmail.com>
2021-11-10 13:06:07 +0100

    styles game/scoreboard pt 1

--- assets/scoreboard.js
+++ assets/scoreboard.js
@@ -1,5 +1,4 @@
 const scoreboard = document.getElementById("scoreboard");
-
 //When loading the window it gets the score stored and shows them in the aside
 window.addEventListener("load", loadScoreboard);
 
@@ -32,14 +31,16 @@ function placeInScoreboard(currentPlayer) {
 
 function loadScoreboard() {
   let playerHistoric = JSON.parse(localStorage.scoreboard);
+  let position=1;
   scoreboard.textContent = "";
   playerHistoric.forEach((element) => {
     console.log(element);
     let newName = document.createElement("h4");
-    newName.innerText = element.name;
+    newName.innerText = position+"º. "+element.name;
     let newScore = document.createElement("p");
     newScore.innerText = element.score + " seconds";
     scoreboard.appendChild(newName);
     scoreboard.appendChild(newScore);
+    position++
   });
 }
